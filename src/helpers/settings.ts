061660c4d4d4f50e1e1323e6f77e6cb4451310eb
Cheerios4316 <accfake7214@gmail.com>
2025-12-21 19:54:11 +0100

    fix crash

--- src/helpers/settings.ts
+++ src/helpers/settings.ts
@@ -6,15 +6,19 @@ import { cookies } from "next/headers";
 export const getSettings = async () => {
   const awaitedCookies = await cookies();
 
-  const awaitedSettings = JSON.parse(
-    awaitedCookies.get("settings")?.value ?? "{}"
-  );
+  const cookieSettings = awaitedCookies.get("settings")?.value;
 
-  if (!awaitedSettings) {
+  try {
+    const awaitedSettings = JSON.parse(cookieSettings ?? "{}");
+
+    if (!awaitedSettings) {
+      return null;
+    }
+
+    return awaitedSettings as ISettings;
+  } catch (error) {
     return null;
   }
-
-  return awaitedSettings as ISettings;
 };
 
 export const getDefaultSettings = async (): Promise<ISettings> => ({
